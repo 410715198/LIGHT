c79af10801d1988d7a24a3c14402ee3bc3729e11
2020上機考版本0(曾薇瑄) <s1071519@gm.pu.edu.tw>
2020-06-05 15:51:53 +0800

    2020上機考版本3(曾薇瑄)

--- app/src/main/java/tw/edu/pu/s1071519/trafficlight/GameSurfaceView.java
+++ app/src/main/java/tw/edu/pu/s1071519/trafficlight/GameSurfaceView.java
@@ -95,6 +95,7 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
             step++;
             if (step>8){
                 step = 1;
+
             }
             int res = getResources().getIdentifier("boy" + (step), "drawable", getContext().getPackageName());
             Boy = BitmapFactory.decodeResource(getResources(), res);
@@ -115,6 +116,7 @@ public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callba
         paint.setTextSize((int) 60 * canvas.getHeight() / 1080);
         paint.setAntiAlias(true);
         canvas.drawText("圖片編號：" + String.valueOf(step), (int) (0), (int) (canvas.getHeight()*0.1) ,paint);
+        canvas.drawText("             ，分數：" + String.valueOf(step), (int) (100), (int) (canvas.getHeight()*0.1) ,paint);
     }
 
     //紅綠燈繪製
